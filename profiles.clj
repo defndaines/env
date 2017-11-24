16b0b78c8ad113224815b878e315176a0b21bee1
Michael Daines <defndaines@gmail.com>
2017-11-23 18:25:44 -0800

    Add .lein/profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,10 @@
+{:user {:plugins [[lein-try "0.4.3"]
+                  [lein-cljsbuild "1.1.7"]
+                  [com.jakemccrary/lein-test-refresh "0.21.1"]
+                  [lein-ancient "0.6.14"]
+                  [lein-cloverage "1.0.10"]
+                  [lein-kibit "0.1.6"]
+                  [lein-bikeshed "0.5.0"]
+                  [nightlight/lein-nightlight "1.9.3"]]}
+ :repl {:dependencies
+        [^:displace [org.clojure/clojure "1.9.0-RC1"]]}}
