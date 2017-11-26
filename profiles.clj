42c79b08b0947e8884fe4df1167831ea563e4982
Michael Daines <michaeld@healthfinch.com>
2017-11-26 09:38:28 -0800

    Unify profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,14 @@
-{:user {:plugins [[lein-try "0.4.3"]
-                  [lein-cljsbuild "1.1.7"]
+{:user {:plugins [[cider/cider-nrepl "0.15.1"]
                   [com.jakemccrary/lein-test-refresh "0.21.1"]
+                  [jonase/eastwood "0.2.5"]
                   [lein-ancient "0.6.14"]
+                  [lein-bikeshed "0.5.0"]
+                  [lein-cljsbuild "1.1.7"]
                   [lein-cloverage "1.0.10"]
                   [lein-kibit "0.1.6"]
-                  [lein-bikeshed "0.5.0"]
-                  [nightlight/lein-nightlight "1.9.3"]]}
+                  [lein-try "0.4.3"]
+                  [lein-typed "0.4.2"]
+                  [nightlight/lein-nightlight "1.9.3"]
+                  [pjstadig/humane-test-output "0.8.3"]]}
  :repl {:dependencies
         [^:displace [org.clojure/clojure "1.9.0-RC1"]]}}
