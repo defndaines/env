565122bd1d9e1bb1294a73607c82c8a9c2ef93be
Michael Daines <953037+defndaines@users.noreply.github.com>
2018-02-17 16:47:45 -0800

    Update profiles.clj and remove unused plugins

--- profiles.clj
+++ profiles.clj
@@ -1,14 +1,13 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.1"]
-                  [com.jakemccrary/lein-test-refresh "0.22.0"]
+{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.22.0"]
                   [jonase/eastwood "0.2.5"]
-                  [lein-ancient "0.6.14"]
-                  [lein-bikeshed "0.5.0"]
-                  [lein-cljsbuild "1.1.7"]
+                  [lein-ancient "0.6.15"]
+                  [lein-bikeshed "0.5.1"]
                   [lein-cloverage "1.0.10"]
                   [lein-kibit "0.1.6"]
                   [lein-try "0.4.3"]
-                  [lein-typed "0.4.2"]
-                  [nightlight/lein-nightlight "2.0.4"]
-                  [pjstadig/humane-test-output "0.8.3"]]}
+                  [lein-typed "0.4.2"]]
+        :dependencies [[pjstadig/humane-test-output "0.8.3"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}
  :repl {:dependencies
         [^:displace [org.clojure/clojure "1.9.0"]]}}
