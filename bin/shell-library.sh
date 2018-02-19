63eb6ac05b7fc465bff1126706859524e9c7bab9
Michael Daines <michaeld@healthfinch.com>
2018-02-19 07:15:39 -0800

    Autocomplete for current psql databases

--- bin/shell-library.sh
+++ bin/shell-library.sh
@@ -55,11 +55,28 @@ function eunit {
 
 # Enable branch completion for `co` alias (git checkout).
 _branches() {
-  local cur branches
+  local cur branches modified
   cur="${COMP_WORDS[COMP_CWORD]}"
   branches=$(git branch --list)
+  # NOTE: checkout -- isn't autocompleting to checked out files.
+  # modified=$(git status --short)
 
-  COMPREPLY=( $(compgen -W "${branches}" -- ${cur}) )
-  return 0
+  # if [[ ${prev} == '--' ]] ; then
+    # COMPREPLY=( $(compgen -W "${modified}" -- ${cur}) )
+    # return 0
+  # else
+    COMPREPLY=( $(compgen -W "${branches}" -- ${cur}) )
+    return 0
+  # fi
 }
 complete -F _branches co
+
+_databases() {
+  local cur dbs
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  dbs=$(psql -l | cut -d\  -f2)
+
+  COMPREPLY=( $(compgen -W "${dbs}" -- ${cur}) )
+  return 0
+}
+complete -F _databases psql
