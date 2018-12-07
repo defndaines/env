3595f9ac68b3ef47208f569853de047ebceac8e4
Michael Daines <michaeld@healthfinch.com>
2018-12-07 08:22:11 -0800

    lein version updates

--- profiles.clj
+++ profiles.clj
@@ -1,20 +1,20 @@
 {:user {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
                   [cider/cider-nrepl "0.18.0"]
-                  [jonase/eastwood "0.3.1"]
+                  [jonase/eastwood "0.3.4"]
                   [lein-ancient "0.6.15"]
                   [lein-bikeshed "0.5.1"]
-                  [lein-cljfmt "0.6.1"]
+                  [lein-cljfmt "0.6.2"]
                   [lein-cloverage "1.0.13"]
                   [lein-kibit "0.1.6"]
                   [lein-try "0.4.3"]
                   [lein-typed "0.4.6"]
-                  [venantius/yagni "0.1.4"]
+                  [venantius/yagni "0.1.6"]
                   ]
-        :dependencies [[pjstadig/humane-test-output "0.8.3"]]
+        :dependencies [[pjstadig/humane-test-output "0.9.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}
  :repl {:dependencies
-        [^:displace [org.clojure/clojure "1.10.0-RC3"]]}
+        [^:displace [org.clojure/clojure "1.10.0-RC4"]]}
  :cljfmt {:remove-consecutive-blank-lines? false
           :indents {fdef [[:inner 0]]}}
 }
