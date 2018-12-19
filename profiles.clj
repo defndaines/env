8f525031eedecb61fb6727215e0ee212febcabc7
Michael Daines <michaeld@healthfinch.com>
2018-12-18 16:16:58 -0800

    Remove obsolete version hack for lein repl

--- profiles.clj
+++ profiles.clj
@@ -14,8 +14,6 @@
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
         :middleware [cider-nrepl.plugin/middleware]}
- :repl {:dependencies
-        [^:displace [org.clojure/clojure "1.10.0"]]}
  :cljfmt {:remove-consecutive-blank-lines? false
           :indents {fdef [[:inner 0]]}}
  }
