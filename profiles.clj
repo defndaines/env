671c3ed331f7e5f414496b459fbdda911607cb78
Michael Daines <michaeld@healthfinch.com>
2019-03-18 11:12:36 -0700

    profiles.clj clean up

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,8 @@
 {:user {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
                   [cider/cider-nrepl "0.21.1"]
-                  [jonase/eastwood "0.3.5"]
+                  [jonase/eastwood "0.3.5" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.15"]
-                  [lein-bikeshed "0.5.1"]
+                  [lein-bikeshed "0.5.1" :exclusions [org.clojure/tools.cli]]
                   [lein-cljfmt "0.6.4"]
                   [lein-cloverage "1.1.1"]
                   [lein-kibit "0.1.6"]
@@ -11,9 +11,6 @@
                   [lein-typed "0.4.6"]
                   [venantius/yagni "0.1.7"]
                   ]
-        :dependencies [[pjstadig/humane-test-output "0.9.0"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]
         :middleware [cider-nrepl.plugin/middleware]}
  :cljfmt {:remove-consecutive-blank-lines? false
           :indents {fdef [[:inner 0]]}}
