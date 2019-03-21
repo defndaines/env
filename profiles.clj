14f416bec1bee46708de060235a6ba266a0e6767
Michael Daines <michaeld@healthfinch.com>
2019-03-21 08:32:34 -0700

    Update lein test-refresh

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
+{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.24.0"]
                   [cider/cider-nrepl "0.21.1"]
                   [jonase/eastwood "0.3.5" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.15"]
