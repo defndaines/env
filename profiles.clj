dc3492a290fd205ed2a854c11a3c257eb09e6ca2
Michael Daines <michaeld@healthfinch.com>
2019-12-17 14:40:01 -0800

    Update lein profiles versions
    Also include how to override JDK version when the latest version breaks
    an older library.

--- profiles.clj
+++ profiles.clj
@@ -3,16 +3,18 @@
                   [jonase/eastwood "0.3.6" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.15"]
                   [lein-bikeshed "0.5.2" :exclusions [org.clojure/tools.cli]]
-                  [lein-cljfmt "0.6.5"]
+                  [lein-cljfmt "0.6.6"]
                   [lein-cloverage "1.1.2"]
                   [lein-kibit "0.1.8"]
-                  [lein-nvd "1.3.0"]
+                  [lein-nvd "1.3.1"]
                   [lein-try "0.4.3"]
                   [lein-typed "0.4.6"]
                   [venantius/yagni "0.1.7"]
                   ]
         :middleware [cider-nrepl.plugin/middleware]
         :dependencies [[clj-kondo "RELEASE"]]
+        ;; For testing older libraries:
+        ; :java-cmd "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin/java"
         :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]}}
  :cljfmt {:remove-consecutive-blank-lines? false
           :indents {fdef [[:inner 0]]}}
