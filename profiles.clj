9fdb38897cd0e02370145328838893b35a14f60b
Michael Daines <michaeld@healthfinch.com>
2020-04-08 13:44:34 -0700

    Updating profiles.clj versions
    Also setting up hashp

--- profiles.clj
+++ profiles.clj
@@ -1,15 +1,17 @@
 {:user {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
                   [cider/cider-nrepl "0.24.0"]
-                  [com.livingsocial/lein-dependency-check  "1.1.4"]
+                  [com.livingsocial/lein-dependency-check "1.1.4"]
                   [lein-ancient "0.6.15"]
-                  [lein-cljfmt "0.6.6"]
+                  [lein-cljfmt "0.6.7"]
                   [lein-cloverage "1.1.2"]
                   [lein-nvd "1.3.1"]
                   [lein-try "0.4.3"]
-                  [lein-zprint "0.5.3"]
+                  [lein-zprint "0.5.4"]
                   [venantius/yagni "0.1.7"]]
+        :injections [(require 'hashp.core)]
         :middleware [cider-nrepl.plugin/middleware]
-        :dependencies [[clj-kondo "RELEASE"]]
+        :dependencies [[clj-kondo "RELEASE"]
+                       [hashp "0.1.1"]]
         ;; For testing older libraries:
         ; :java-cmd "/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/java"
         ; :java-cmd "/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home/bin/java"
