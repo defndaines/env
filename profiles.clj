ec1a058c89b4ff83e1809eaa8dacdb2a28ff99ce
Michael Daines <michaeld@healthfinch.com>
2020-12-31 07:30:16 -0800

    profiles.clj updates

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
-                  [cider/cider-nrepl "0.25.4"]
+                  [cider/cider-nrepl "0.25.5"]
                   [com.livingsocial/lein-dependency-check "1.1.5"]
                   [lein-ancient "0.6.15"]
                   [lein-cljfmt "0.7.0"]
