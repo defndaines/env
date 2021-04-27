99710193824746ac63cf1d339d442c9a58e079fb
Michael Daines <michaeld@healthfinch.com>
2021-04-27 09:50:55 -0700

    Update cider-nrepl version

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
-                  [cider/cider-nrepl "0.25.10"]
+                  [cider/cider-nrepl "0.26.0"]
                   [com.livingsocial/lein-dependency-check "1.1.5"]
                   [lein-ancient "0.7.0"]
                   [lein-cljfmt "0.7.0"]
